47a992a014866f7507bbd30370cd6315e701308e
SiGua <1058435977@qq.com>
2018-12-14 17:00:38 +0800

    Ch14

---
+++ KarliCards GUI/GameOptions.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace KarliCards_GUI
+{
+    [Serializable]
+    public class GameOptions
+    {
+        private ObservableCollection<string> _playerNames = new ObservableCollection<string>();
+        public ObservableCollection<string> PlayerNames
+        {
+            get => _playerNames;
+            set
+            {
+                _playerNames = value;
+                OnPropertyChanged("PlayerNames");
+            }
+        }
+
+        public void AddPlayer(string playerName)
+        {
+            if (_playerNames.Contains(playerName))
+                return;
+            _playerNames.Add(playerName);
+            OnPropertyChanged("PlayerNames");
+        }
+
+        public List<string> SelectedPlayers { get; set; }
+        public GameOptions()
+        {
+            SelectedPlayers = new List<string>;
+        }
+
+
+        private bool _playAgainstComputer = true;
+        private int _numberOfPlayers = 2;
+        private ComputerSkillLevel _computerSkill = ComputerSkillLevel.Dumb;
+        public bool PlayAgainstComputer
+        {
+            get { return _playAgainstComputer; }
+            set
+            {
+                _playAgainstComputer = value;
+                OnPropertyChanged(nameof(PlayAgainstComputer));
+            }
+        }
+        public int NumberOfPlayers
+        {
+            get { return _numberOfPlayers; }
+            set
+            {
+                _numberOfPlayers = value;
+                OnPropertyChanged(nameof(NumberOfPlayers));
+            }
+        }
+        public int MinutesBeforeLoss { get; set; }
+        public ComputerSkillLevel ComputerSkill
+        {
+            get { return _computerSkill; }
+            set
+            {
+                _computerSkill = value;
+                OnPropertyChanged(nameof(ComputerSkill));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+    [Serializable]
+    public enum ComputerSkillLevel
+    {
+        Dumb,
+        Good,
+        Cheats
+    }
+}
---
+++ KarliCards GUI/Options.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KarliCards_GUI
+{
+    /// <summary>
+    /// Options.xaml 的交互逻辑
+    /// </summary>
+    public partial class Options : Window
+    {
+        private GameOptions _gameOptions;
+        public Options()
+        {
+            if (_gameOptions == null)
+            {
+                using (var stream = File.OpenRead("GameOptions.xml"))
+                {
+                    var serializer = new XmlSerializer(typeof(GameOptions));
+                    _gameOptions = serializer.Deserialize(stream) as GameOptions;
+                }
+            }
+            else  // */
+                _gameOptions = new GameOptions();
+
+            InitializeComponent();
+            DataContext = _gameOptions;
+        }
+
+        private void DumbRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            _gameOptions.ComputerSkill = ComputerSkillLevel.Dumb;
+        }
+
+        private void GoodAIRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            _gameOptions.ComputerSkill = ComputerSkillLevel.Good;
+        }
+
+        private void CheatingAIRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            _gameOptions.ComputerSkill = ComputerSkillLevel.Cheats;
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var stream = File.Open("GameOptions.xml", FileMode.Create))
+            {
+                var serializer = new XmlSerializer(typeof(GameOptions));
+                serializer.Serialize(stream, _gameOptions);
+            }
+            Close();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            _gameOptions = null;
+            Close();
+        }
+    }
+}
