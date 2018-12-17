8e2f39531eeec67567e02531b6b58d2d49301fe0
SiGua <1058435977@qq.com>
2018-12-17 22:31:13 +0800

    1. 绑定Options Comb

---
+++ KarliCards GUI/ReversedBoolConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Data;
+
+namespace KarliCards_GUI
+{
+    [ValueConversion(typeof(bool), typeof(bool))]
+    class ReversedBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            //throw new NotImplementedException();
+            return !(bool)value;
+        }
+    }
+}
